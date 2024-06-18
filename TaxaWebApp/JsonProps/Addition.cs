75e7a03ffe8e595450a7a245a0de93912428d4c9
Malth <Malthe@johannes@hotmail.com>
2024-06-18 09:04:18 +0200

    finished

---
+++ TaxaWebApp/JsonProps/Addition.cs
@@ -0,0 +1,9 @@
+﻿namespace TaxaWebApp
+{
+    public class Addition
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; } = 0;
+    }
+}
--- TaxaWebApp/JsonProps/TaxiRate.cs
+++ TaxaWebApp/JsonProps/TaxiRate.cs
@@ -6,6 +6,6 @@
         public string? VehicleType { get; set; }
         public string? Time { get; set; }
         public Dictionary<string, string>? Drive { get; set; }
-        public Dictionary<string, string>? Addition { get; set; }
+        public List<Addition>? Additions { get; set; }
     }
 }
